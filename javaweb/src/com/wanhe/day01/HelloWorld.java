299942d95481645314500e55313ad4e2c195f400
RonHao8 <1339509751@qq.com>
2022-09-19 20:25:51 +0800

    day01

---
+++ javaweb/src/com/wanhe/day01/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.wanhe.day01;
+
+//public:是一个访问权限修饰符，表明当前的类是公共类，可以被其他类所调用
+//class:定义一个类,java程序的基本单位就是类，后面跟的是类名名称（自定义）
+public class HelloWorld {
+    //程序入口（简单记忆），main方法（主方法）psvm
+    /*
+    * 多行注释
+    * 注释的内容是不会参与编译和运行
+    *
+    */
+    public static void main(String[] args) {
+        //执行的业务逻辑 sout
+        System.out.println("java入门");//输出内容到控制台
+    }
+}
