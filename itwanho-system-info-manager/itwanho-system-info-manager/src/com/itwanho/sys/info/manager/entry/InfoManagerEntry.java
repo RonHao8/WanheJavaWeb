c5c7e0a7b9304f80629366183cacc292e39b55a9
RonHao8 <1339509751@qq.com>
2022-09-30 21:47:05 +0800

    third commit

--- itwanho-system-info-manager/itwanho-system-info-manager/src/com/itwanho/sys/info/manager/entry/InfoManagerEntry.java
+++ itwanho-system-info-manager/itwanho-system-info-manager/src/com/itwanho/sys/info/manager/entry/InfoManagerEntry.java
@@ -1,36 +1,37 @@
 package com.itwanho.sys.info.manager.entry;
 
 import com.itwanho.sys.info.manager.controller.StudentController;
+import com.itwanho.sys.info.manager.controller.TeacherController;
 
 import java.util.Scanner;
 
-/**
- * 入口
- */
 public class InfoManagerEntry {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true){
-            //主菜单搭建
+        while (true) {
+            // 主菜单搭建
             System.out.println("--------欢迎来到万和信息管理系统--------");
             System.out.println("请输入您的选择: 1.学生管理  2.老师管理  3.退出");
             String choice = sc.next();
-            switch (choice){
+            switch (choice) {
                 case "1":
-                    System.out.println("学生管理");
-                    //开启学生管理系统
+                    // System.out.println("学生管理");
+                    // 开启学生管理系统
                     StudentController studentController = new StudentController();
                     studentController.start();
                     break;
                 case "2":
                     System.out.println("老师管理");
+                    TeacherController teacherController = new TeacherController();
+                    teacherController.start();
                     break;
                 case "3":
                     System.out.println("感谢您的使用");
-                    System.exit(0);//退出虚拟机
+                    // 退出当前正在运行的JVM虚拟机
+                    System.exit(0);
                     break;
                 default:
-                    System.out.println("您的输入有误，请重新输入");
+                    System.out.println("您的输入有误, 请重新输入");
                     break;
             }
         }
